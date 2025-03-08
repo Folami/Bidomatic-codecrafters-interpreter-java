78890e56ca73c1fb52fa9168a7f462ca7c7ee67c
Folami <folami.bidmus@gmail.com>
2025-03-08 18:26:08 +0100

    [init commit]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -39,6 +39,8 @@ public class Main {
         }
       }
       System.out.println("EOF  null");
-    } 
+    } else {
+      System.out.println("EOF  null"); // Placeholder, remove this line when implementing the scanner
+    }
   }
 }
