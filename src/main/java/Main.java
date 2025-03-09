510bfec2f97fcea7146cd6a6dd1d044120b9b0aa
Folami <folami.bidmus@gmail.com>
2025-03-09 11:15:00 +0100

    [init commit]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,11 +26,14 @@ public class Main {
             System.err.println("Error reading file: " + e.getMessage());
             System.exit(1);
         }
+
         boolean hasError = false;
         int lineNumber = 1;
         int index = 0;
+
         while (index < fileContents.length()) {
             char ch = fileContents.charAt(index);
+
             if (ch == '\n') {
                 lineNumber++;
             } else if (ch == '/') { // Check for comments
@@ -43,58 +46,91 @@ public class Main {
                     lineNumber++;
                     continue;
                 }
-            } else if ("() {}*+-.,;".indexOf(ch) != -1) {
+            } else if ("(){}*+-.,;".indexOf(ch) != -1) {
                 // Valid single-character token handling remains the same ...
                 switch (ch) {
-                  case '(':
-                      System.out.println("LEFT_PAREN ( null");
-                      break;
-                  case ')':
-                      System.out.println("RIGHT_PAREN ) null");
-                      break;
-                  case '{':
-                      System.out.println("LEFT_BRACE { null");
-                      break;
-                  case '}':
-                      System.out.println("RIGHT_BRACE } null");
-                      break;
-                  case '*':
-                      System.out.println("STAR * null");
-                      break;
-                  case '+':
-                      System.out.println("PLUS + null");
-                      break;
-                  case '-':
-                      System.out.println("MINUS - null");
-                      break;
-                  case ',':
-                      System.out.println("COMMA , null");
-                      break;
-                  case '.':
-                      System.out.println("DOT . null");
-                      break;
-                  case ';':
-                      System.out.println("SEMICOLON ; null");
-                      break;
-              }
+                    case '(':
+                        System.out.println("LEFT_PAREN ( null");
+                        break;
+                    case ')':
+                        System.out.println("RIGHT_PAREN ) null");
+                        break;
+                    case '{':
+                        System.out.println("LEFT_BRACE { null");
+                        break;
+                    case '}':
+                        System.out.println("RIGHT_BRACE } null");
+                        break;
+                    case '*':
+                        System.out.println("STAR * null");
+                        break;
+                    case '+':
+                        System.out.println("PLUS + null");
+                        break;
+                    case '-':
+                        System.out.println("MINUS - null");
+                        break;
+                    case ',':
+                        System.out.println("COMMA , null");
+                        break;
+                    case '.':
+                        System.out.println("DOT . null");
+                        break;
+                    case ';':
+                        System.out.println("SEMICOLON ; null");
+                        break;
+                }
             } else if (Character.isWhitespace(ch)) {
                 // Ignore whitespace
-                // case ' ':
-                // case '\t':
-                // case '\r':
-                //       break;
+            } else if (ch == '"') {
+                // Handle string literals
+                StringBuilder stringLiteral = new StringBuilder();
+                index++;
+                boolean unterminated = true;
+                while (index < fileContents.length()) {
+                    ch = fileContents.charAt(index);
+                    if (ch == '"') {
+                        unterminated = false;
+                        break;
+                    }
+                    if (ch == '\n') {
+                        lineNumber++;
+                    }
+                    stringLiteral.append(ch);
+                    index++;
+                }
+                if (unterminated) {
+                    System.err.println("[line " + lineNumber + "] Error: Unterminated string.");
+                    hasError = true;
+                } else {
+                    System.out.println("STRING \"" + stringLiteral.toString() + "\" null");
+                }
+            } else if (Character.isDigit(ch)) {
+                // Handle number literals
+                StringBuilder numberLiteral = new StringBuilder();
+                while (index < fileContents.length() && Character.isDigit(fileContents.charAt(index))) {
+                    numberLiteral.append(fileContents.charAt(index));
+                    index++;
+                }
+                index--; // Adjust for the loop increment
+                System.out.println("NUMBER " + numberLiteral.toString() + " null");
+            } else if (Character.isLetter(ch) || ch == '_') {
+                // Handle identifiers
+                StringBuilder identifier = new StringBuilder();
+                while (index < fileContents.length() && (Character.isLetterOrDigit(fileContents.charAt(index)) || fileContents.charAt(index) == '_')) {
+                    identifier.append(fileContents.charAt(index));
+                    index++;
+                }
+                index--; // Adjust for the loop increment
+                System.out.println("IDENTIFIER " + identifier.toString() + " null");
             } else {
-                // Handle invalid character; skip rest of line
+                // Handle invalid character
                 hasError = true;
                 System.err.println("[line " + lineNumber + "] Error: Unexpected character: " + ch);
-                index = fileContents.indexOf('\n', index + 1);
-                if (index == -1) {
-                    index = fileContents.length();
-                }
-                continue; // Skip to the beginning of the next line
             }
             index++;
         }
+
         System.out.println("EOF  null");
         if (hasError) {
             System.exit(65);
