34e3955bb76d9a672c39609a56a3490fe7ac9543
Folami <folami.bidmus@gmail.com>
2025-03-09 15:16:00 +0100

    [init commit]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -158,57 +158,143 @@ public class Main {
         return index;
     }
 
-    private static boolean handleRelationalOperator(String fileContents, int index, int lineNumber) {
-       if (index + 1 >= fileContents.length()) {
-            return false;
+    private static void tokenize(String fileContents) {
+        boolean hasError = false;
+        int lineNumber = 1;
+        int index = 0;
+        while (index < fileContents.length()) {
+            char ch = fileContents.charAt(index);
+            if (ch == '\n') {
+                lineNumber++;
+                index++;
+                continue;
+            }
+            if (ch == '/' && index + 1 < fileContents.length() && fileContents.charAt(index + 1) == '/') {
+                index = handleComment(fileContents, index);
+                continue;
+            }
+            if ("(){}*+-.,;".indexOf(ch) != -1) {
+                handleSingleCharacterToken(ch);
+                index++;
+                continue;
+            }
+            if (Character.isWhitespace(ch)) {
+                index++;
+                continue;
+            }
+            if (ch == '"') {
+                index = handleStringLiteral(fileContents, index, lineNumber);
+                continue;
+            }
+            if (Character.isDigit(ch)) {
+                index = handleNumberLiteral(fileContents, index);
+                continue;
+            }
+            if (Character.isLetter(ch) || ch == '_') {
+                index = handleIdentifier(fileContents, index);
+                continue;
+            }
+            int consumed = handleRelationalOperator(fileContents, index, lineNumber);
+            if (consumed > 0) {
+                index += consumed;
+                continue;
+            }
+            consumed = handleAssignmentOrEqualityOperator(fileContents, index, lineNumber);
+            if (consumed > 0) {
+                index += consumed;
+                continue;
+            }
+            consumed = handleNegationOrInequalityOperator(fileContents, index, lineNumber);
+            if (consumed > 0) {
+                index += consumed;
+                continue;
+            }
+            System.err.println("[line " + lineNumber + "] Error: Unexpected character: " + ch);
+            hasError = true;
+            index++;
+        }
+        System.out.println("EOF  null");
+        System.exit(hasError ? 65 : 0);
+    }
+
+    private static int handleRelationalOperator(String fileContents, int index, int lineNumber) {
+        /*
+         * Handles relational operators (<, >, <=, >=).
+         * Returns: Number of characters consumed (1 or 2), or 0 if no match.
+         */
+        if (index + 1 >= fileContents.length()) {
+            if (fileContents.charAt(index) == '<') {
+                System.out.println("LESS < null");
+                return 1;
+            } else if (fileContents.charAt(index) == '>') {
+                System.out.println("GREATER > null");
+                return 1;
+            }
+            return 0;
         }
         char c1 = fileContents.charAt(index);
         char c2 = fileContents.charAt(index + 1);
         if (c1 == '<' && c2 == '=') {
             System.out.println("LESS_EQUAL <= null");
-            return true;
+            return 2;
         } else if (c1 == '>' && c2 == '=') {
             System.out.println("GREATER_EQUAL >= null");
-            return true;
+            return 2;
         } else if (c1 == '<') {
             System.out.println("LESS < null");
-            return true;
+            return 1;
         } else if (c1 == '>') {
             System.out.println("GREATER > null");
-            return true;
+            return 1;
         }
-        return false;
+        return 0;
     }
 
-    private static boolean handleAssignmentOrEqualityOperator(String fileContents, int index, int lineNumber) {
+    private static int handleAssignmentOrEqualityOperator(String fileContents, int index, int lineNumber) {
+        /*
+         * Handles assignment and equality operators (=, ==).
+         * Returns: Number of characters consumed (1 or 2), or 0 if no match.
+         */
         if (index + 1 >= fileContents.length()) {
-            return false;
+            if (fileContents.charAt(index) == '=') {
+                System.out.println("EQUAL = null");
+                return 1;
+            }
+            return 0;
         }
         char c1 = fileContents.charAt(index);
         char c2 = fileContents.charAt(index + 1);
         if (c1 == '=' && c2 == '=') {
             System.out.println("EQUAL_EQUAL == null");
-            return true;
+            return 2;
         } else if (c1 == '=') {
             System.out.println("EQUAL = null");
-            return true;
+            return 1;
         }
-        return false;
+        return 0;
     }
 
-    private static boolean handleNegationOrInequalityOperator(String fileContents, int index, int lineNumber) {
+    private static int handleNegationOrInequalityOperator(String fileContents, int index, int lineNumber) {
+        /*
+         * Handles negation and inequality operators (!, !=).
+         * Returns: Number of characters consumed (1 or 2), or 0 if no match.
+         */
         if (index + 1 >= fileContents.length()) {
-            return false;
+            if (fileContents.charAt(index) == '!') {
+                System.out.println("BANG ! null");
+                return 1;
+            }
+            return 0;
         }
         char c1 = fileContents.charAt(index);
         char c2 = fileContents.charAt(index + 1);
         if (c1 == '!' && c2 == '=') {
             System.out.println("BANG_EQUAL != null");
-            return true;
+            return 2;
         } else if (c1 == '!') {
             System.out.println("BANG ! null");
-            return true;
+            return 1;
         }
-        return false;
+        return 0;
     }
 }
