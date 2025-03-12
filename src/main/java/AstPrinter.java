388d450af1a6a5c8783d230d5c6e3ced5b9591f2
Folami <folami.bidmus@gmail.com>
2025-03-12 15:05:49 +0100

    [init commit]

--- src/main/java/AstPrinter.java
+++ src/main/java/AstPrinter.java
@@ -8,6 +8,42 @@ class AstPrinter implements Expr.Visitor<String> {
     String print(Expr expr) {
       return expr.accept(this);
     }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+      return parenthesize(expr.operator.lexeme,
+                          expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+      return parenthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+      if (expr.value == null) return "nil";
+      return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+      return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    private String parenthesize(String name, Expr... exprs) {
+      StringBuilder builder = new StringBuilder();
+
+      builder.append("(").append(name);
+      for (Expr expr : exprs) {
+        builder.append(" ");
+        builder.append(expr.accept(this));
+      }
+      builder.append(")");
+
+      return builder.toString();
+    }
+
     /* 
     String print(Stmt stmt) {
       return stmt.accept(this);
@@ -97,12 +133,6 @@ class AstPrinter implements Expr.Visitor<String> {
     public String visitAssignExpr(Expr.Assign expr) {
       return parenthesize2("=", expr.name.lexeme, expr.value);
     }
-*/
-    @Override
-    public String visitBinaryExpr(Expr.Binary expr) {
-      return parenthesize(expr.operator.lexeme,
-                          expr.left, expr.right);
-    }
     
     @Override
     public String visitCallExpr(Expr.Call expr) {
@@ -112,17 +142,6 @@ class AstPrinter implements Expr.Visitor<String> {
     @Override
     public String visitGetExpr(Expr.Get expr) {
       return parenthesize2(".", expr.object, expr.name.lexeme);
-    }
-    
-    @Override
-    public String visitGroupingExpr(Expr.Grouping expr) {
-      return parenthesize("group", expr.expression);
-    }
-
-    @Override
-    public String visitLiteralExpr(Expr.Literal expr) {
-      if (expr.value == null) return "nil";
-      return expr.value.toString();
     }
 
     @Override
@@ -147,25 +166,8 @@ class AstPrinter implements Expr.Visitor<String> {
     }
 
     @Override
-    public String visitUnaryExpr(Expr.Unary expr) {
-      return parenthesize(expr.operator.lexeme, expr.right);
-    }
-
-    @Override
     public String visitVariableExpr(Expr.Variable expr) {
       return expr.name.lexeme;
-    }
-    private String parenthesize(String name, Expr... exprs) {
-      StringBuilder builder = new StringBuilder();
-
-      builder.append("(").append(name);
-      for (Expr expr : exprs) {
-        builder.append(" ");
-        builder.append(expr.accept(this));
-      }
-      builder.append(")");
-
-      return builder.toString();
     }
 
     private String parenthesize2(String name, Object... parts) {
@@ -195,7 +197,7 @@ class AstPrinter implements Expr.Visitor<String> {
       }
     }
 
-  /* Representing Code printer-main < Representing Code omit
+    // Representing Code printer-main < Representing Code omit
     public static void main(String[] args) {
       Expr expression = new Expr.Binary(
           new Expr.Unary(
