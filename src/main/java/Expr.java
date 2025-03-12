b39a69aca7960c31e098afc50797cd14ec1f97e7
Folami <folami.bidmus@gmail.com>
2025-03-12 15:50:44 +0100

    [init commit]

--- src/main/java/Expr.java
+++ src/main/java/Expr.java
@@ -2,7 +2,7 @@
 // package com.craftinginterpreters.lox;
 
 import java.util.List;
-// import com.craftinginterpreters.lox.Main.LoxScanner.Token;
+import Main.LoxScanner.Token;
 
 abstract class Expr {
     interface Visitor<R> {
