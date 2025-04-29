469c314d174aa9809176e13d8720c305a6a81c4c
Folami <folami.bidmus@gmail.com>
2025-04-29 18:10:36 +0100

    [Class Declarations]

--- src/main/java/Stmt.java
+++ src/main/java/Stmt.java
@@ -149,7 +149,7 @@ abstract class Stmt {
     static class Class extends Stmt {
         Class(Token name, List<Stmt.Function> methods) {
             this.name = name;
-            this.superclass = superclass;
+            // this.superclass = superclass;
             this.methods = methods;
         }
 
@@ -159,7 +159,7 @@ abstract class Stmt {
         }
 
         final Token name;
-        final Expr.Variable superclass;
+        // final Expr.Variable superclass;
         final List<Stmt.Function> methods;
     }
 
