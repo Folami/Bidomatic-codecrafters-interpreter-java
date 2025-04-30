640fa7798cd3d1a41b977f37feb03f6982434d23
Folami <folami.bidmus@gmail.com>
2025-04-30 12:18:37 +0100

    [Constructor calls]

--- src/main/java/LoxFunction.java
+++ src/main/java/LoxFunction.java
@@ -8,9 +8,30 @@ class LoxFunction implements LoxCallable {
     private final boolean isInitializer;
 
     LoxFunction(Stmt.Function declaration, Environment closure, boolean isInitializer) {
-        this.isInitializer = isInitializer;
-        this.closure = closure;
         this.declaration = declaration;
+        this.closure = closure;
+        this.isInitializer = isInitializer;
+    }
+
+    @Override
+    public int arity() {
+        return declaration.params.size();
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        Environment environment = new Environment(closure);
+        for (int i = 0; i < declaration.params.size(); i++) {
+            environment.define(declaration.params.get(i).lexeme, arguments.get(i));
+        }
+        try {
+            interpreter.executeBlock(declaration.body, environment);
+        } catch (Return returnValue) {
+            if (isInitializer) return closure.getAt(0, "this");
+            return returnValue.value;
+        }
+        if (isInitializer) return closure.getAt(0, "this");
+        return null;
     }
 
     LoxFunction bind(LoxInstance instance) {
@@ -20,29 +41,7 @@ class LoxFunction implements LoxCallable {
     }
 
     @Override
-    public int arity() {
-        return declaration.params.size();
-    }
-
-    @Override
     public String toString() {
         return "<fn " + declaration.name.lexeme + ">";
-    }
-
-    @Override
-    public Object call(Interpreter interpreter,
-                        List<Object> arguments) {
-        Environment environment = new Environment(closure);
-        for (int i = 0; i < declaration.params.size(); i++) {
-            environment.define(declaration.params.get(i).lexeme, arguments.get(i));
-        }
-        try {
-            interpreter.executeBlock(declaration.body, environment);
-        } catch (Return returnValue) {
-            if (isInitializer) 
-                return closure.getAt(0, "this");
-            return returnValue.value;
-        }
-        return null;
     }
 }
